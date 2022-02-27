8c9fba2e6668114ccdfbe66b4666a55e8a83dc82
Yangtao Ge <yangtao.ge.17@ucl.ac.uk>
2022-02-27 22:28:00 +0000

    add list of users for test

--- service/auth-service.go
+++ service/auth-service.go
@@ -1,22 +1,42 @@
 package service
 
+import (
+	"leaky-image-project/chat-api/entity"
+)
+
 type AuthService interface {
 	Login(username string, password string) bool
 }
 
 type authService struct {
-	authorizedUsername string
-	authorizedPassword string
+	credentialList []entity.User
 }
 
 func NewAuthService() AuthService {
 	return &authService{
-		authorizedUsername: "attacker",
-		authorizedPassword: "passAttack",
+		credentialList: []entity.User{
+			{
+				Name:     "attacker",
+				Password: "attackPass",
+			},
+			{
+				Name:     "victim0",
+				Password: "victim0Pass",
+			},
+			{
+				Name:     "victim1",
+				Password: "victim1Pass",
+			},
+		},
 	}
 }
 
 func (service *authService) Login(username string, password string) bool {
-	return service.authorizedUsername == username &&
-		service.authorizedPassword == password
+	for _, v := range service.credentialList {
+		if v.Name == username && v.Password == password {
+			return true
+		}
+	}
+
+	return false
 }
